82052491ebd6fbad6d6d000e1003347d88fae07a
Paul121121121 <myfishisadog@gmail.com>
2026-09-01 21:27:08 +0300

    Delete test-util.js

--- test-util.js
+++
@@ -1,3 +0,0 @@
-// Test helpers. The navmesh and pathfinder live in public/nav.js, because the
-// bots use them too - there is no point having two copies that can disagree.
-module.exports = require('./public/nav.js');
